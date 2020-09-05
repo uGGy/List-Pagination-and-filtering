fa14f156dabd402a8009aed6529b04d920a39505
Alexis Vogelsperger <47717401+uGGy@users.noreply.github.com>
2020-09-05 10:58:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,36 +5,16 @@ FSJS project 2 - List Filter and Pagination
 
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
+// Var stocking the list of student
 
 const studentList = document.querySelectorAll(".student-item");
 
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
+// Var stocking the indexes
 
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
 let startIndex = 0;
 let endIndex = 9;
 
+// Function showPage using Tenary Operator
 
 function showPage(list, page) {
 
@@ -42,7 +22,7 @@ function showPage(list, page) {
   startIndex =  0 + ((page - 1) * 10)) : (endIndex, startIndex);
 
 
-
+  // Loop passing through the student list displaying 10 items per page and hidding the rest
   list = studentList;
   for (let i = 0; i < list.length; i++) {
     if (i >= startIndex && i <= endIndex) {
@@ -53,28 +33,17 @@ function showPage(list, page) {
   }
 }
 
-// /***
-//    Create the `appendPageLinks function` to generate, append, and add
-//    functionality to the pagination buttons.
-// ***/
+
+//  Function to generate and append links & functionality to the pagination buttons.
 
 function appendPageLinks(list) {
-  /*
-   1. Determine how many pages are needed for the list by dividing the
-   total number of list items by the max number of items per page
-   2. Create a div, give it the “pagination” class, and append it to the .page div
-   3. Add a ul to the “pagination” div to store the pagination links
-   4. for every page, add li and a tags with the page number text
-   5. Add an event listener to each a tag. When they are clicked
-   call the showPage function to display the appropriate page
-   6. Loop over pagination links to remove active class from all links
-   7. Add the active class to the link that was just clicked. You can identify that
-   clicked link using event.target
-   */
+
 
   list = studentList;
   let currentPage = 1;
   const NumberOfPage = (Math.ceil(studentList.length / 10));
+
+  // Positioning the button in a ul element, which will contain the button at the bottom of the page
 
   let divPagination = document.createElement('div');
   divPagination.classList.add("pagination");
@@ -87,19 +56,29 @@ function appendPageLinks(list) {
   let li = document.createElement('li');
   ul.appendChild(li);
 
+
+ // Loop passing through the number of pages and creating one button for each page
+
   for (let i = 1; i < NumberOfPage + 1; i++) {
     let a = document.createElement('a');
     a.setAttribute("href", "#");
     a.innerText = i;
     li.appendChild(a);
 
+    // Adds class 'ative' list to the first page
+
     if (parseInt(a.innerText) === currentPage) {
       a.classList.add('active');
     }
 
+    // Event listener that display the list of when the button link/button is clicked
+  
     a.addEventListener('click', (event) => {
+
       let links = document.querySelectorAll('a');
       let currentPage = parseInt(event.target.innerText);
+
+      // Loop passing through each page, the class 'active' is added to the current page and remove from the others
 
       for (let x = 0; x < links.length; x++) {
         const pages = links[x];
@@ -112,12 +91,20 @@ function appendPageLinks(list) {
 
         }
       }
+      // Function that displays the list per page is called
       showPage(studentList, currentPage);
 
     });
   }
+
+  // Function that displays/hides the list of items per page is called
+
   showPage(studentList, currentPage);
 
 }
+
+//Function to generate and append links & functionality to the pagination buttons is called to activate it
 appendPageLinks(studentList);
+
+
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
